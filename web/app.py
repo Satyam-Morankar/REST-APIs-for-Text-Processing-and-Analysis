63944d4e7864643a9d332c5c0bd259017f5ad7d1
nahorh <haburohan@gmail.com>
2022-11-23 15:29:51 +0530

    23/11/22

--- web/app.py
+++ web/app.py
@@ -1,21 +1,21 @@
+from nltk.tokenize import sent_tokenize, word_tokenize
+import os
+import re
+import nltk
+import heapq
+from nltk.corpus import stopwords
+import spacy
 from flask import Flask, jsonify, request
 from flask_restful import Api, Resource
 from pymongo import MongoClient
 import bcrypt
 import language_tool_python
-# using the tool  
-my_tool = language_tool_python.LanguageTool('en-US')  
-import spacy
+# using the tool
+my_tool = language_tool_python.LanguageTool('en-US')
 nlp = spacy.load('en_core_web_sm')
 
 # ------------------------------------------
 # summarization depecdancies
-from nltk.corpus import stopwords
-from nltk.tokenize import sent_tokenize,word_tokenize
-import heapq
-import nltk
-import re
-import os
 # ------------------------------------------
 
 app = Flask(__name__)
@@ -25,38 +25,40 @@ client = MongoClient("mongodb://localhost:27017")
 db = client.SimilarityDB
 users = db["Users"]
 
-base_path=os.getcwd()
+base_path = os.getcwd()
+
 
 def UserExist(username):
     # if users.find({"Username":username}).count() == 0:
-    if users.count_documents({"Username":username}) == 0:
+    if users.count_documents({"Username": username}) == 0:
         return False
     else:
         return True
 
+
 class Register(Resource):
     def post(self):
-        #Step 1 is to get posted data by the user
+        # Step 1 is to get posted data by the user
         postedData = request.get_json()
 
-        #Get the data
+        # Get the data
         username = postedData["username"]
-        password = postedData["password"] #"123xyz"
+        password = postedData["password"]  # "123xyz"
 
         if UserExist(username):
             retJson = {
-                'status':301,
+                'status': 301,
                 'msg': 'Invalid Username'
             }
             return jsonify(retJson)
 
         hashed_pw = bcrypt.hashpw(password.encode('utf8'), bcrypt.gensalt())
 
-        #Store username and pw into the database
+        # Store username and pw into the database
         users.insert_one({
             "Username": username,
             "Password": hashed_pw,
-            "Tokens":6
+            "Tokens": 6
         })
 
         retJson = {
@@ -64,12 +66,14 @@ class Register(Resource):
             "msg": "You have successfully signed up for the API"
         }
         return jsonify(retJson)
+
+
 def verifyPw(username, password):
     if not UserExist(username):
         return False
 
     hashed_pw = users.find({
-        "Username":username
+        "Username": username
     })[0]["Password"]
 
     if bcrypt.hashpw(password.encode('utf8'), hashed_pw) == hashed_pw:
@@ -77,38 +81,41 @@ def verifyPw(username, password):
     else:
         return False
 
+
 def countTokens(username):
     tokens = users.find({
-        "Username":username
+        "Username": username
     })[0]["Tokens"]
     return tokens
+
 
 def summarize(text):
     pass
 
-def summarize_text(filename,no_of_sentences,print_summarized_text=False):
+
+def summarize_text(filename, no_of_sentences=5, print_summarized_text=False):
     # read the text file
-    path=base_path+"\\documents"+"\\"+filename
+    path = base_path+"\\documents"+"\\"+filename
     print(path)
     if not os.path.exists(path):
-        retJson={"status":304,
-                 "msg":"File not found"}
+        retJson = {"status": 304,
+                   "msg": "File not found"}
         return jsonify(retJson)
     with open(path) as txt:
-        data=txt.read()
+        data = txt.read()
     # sentence tokenization
     sentences = sent_tokenize(data)
     print('Total number of sentences in the text: {0}'.format(len(sentences)))
-    
+
     # cleaning text
     dict = {}
-    text=""
+    text = ""
     for a in sentences:
-        temp = re.sub("[^a-zA-Z]"," ",a)
+        temp = re.sub("[^a-zA-Z]", " ", a)
         temp = temp.lower()
         dict[temp] = a
-        text+=temp
-        
+        text += temp
+
     # stop words removal
     stopwords = nltk.corpus.stopwords.words('english')
     word_fre = {}
@@ -118,13 +125,13 @@ def summarize_text(filename,no_of_sentences,print_summarized_text=False):
                 word_fre[word] = 1
             else:
                 word_fre[word] += 1
-                
+
     max_freq = max(word_fre.values())
-    
+
     # calculating word frequencies in the text
-    for w in word_fre :
-        word_fre[w]/=max_freq
-        
+    for w in word_fre:
+        word_fre[w] /= max_freq
+
     # calculating sentence scores based on word frequencies
     sent_scores = {}
     for sent in sentences:
@@ -135,9 +142,10 @@ def summarize_text(filename,no_of_sentences,print_summarized_text=False):
                         sent_scores[sent] = word_fre[word]
                     else:
                         sent_scores[sent] += word_fre[word]
-                        
+
     # summary generation using top 'n' sentences from sent_scores
-    summarized_sentences = heapq.nlargest(no_of_sentences, sent_scores, key=sent_scores.get)
+    summarized_sentences = heapq.nlargest(
+        no_of_sentences, sent_scores, key=sent_scores.get)
     summarized_text = ' '.join(summarized_sentences)
     if print_summarized_text:
         print('-'*80)
@@ -149,41 +157,43 @@ def summarize_text(filename,no_of_sentences,print_summarized_text=False):
         print('-'*80)
         print(summarized_text)
         print('-'*80)
-    return data,summarized_text
+    return data, summarized_text
 
-def calculate_reading_time(text,wpm=120):
-    tokens=nltk.word_tokenize(text)
+
+def calculate_reading_time(text, wpm=120):
+    tokens = nltk.word_tokenize(text)
     return len(tokens)/wpm
+
 
 class Similarity(Resource):
     def post(self):
-        #Step 1 get the posted data
+        # Step 1 get the posted data
         postedData = request.get_json()
 
-        #Step 2 is to read the data
+        # Step 2 is to read the data
         username = postedData["username"]
         password = postedData["password"]
         path1 = postedData["path1"]
-        path1=base_path+"\\documents"+"\\"+path1
+        path1 = base_path+"\\documents"+"\\"+path1
         path2 = postedData["path2"]
-        path2=base_path+"\\documents"+"\\"+path2
+        path2 = base_path+"\\documents"+"\\"+path2
 
         if not UserExist(username):
             retJson = {
-                'status':301,
+                'status': 301,
                 'msg': "Invalid Username"
             }
             return jsonify(retJson)
-        #Step 3 verify the username pw match
+        # Step 3 verify the username pw match
         correct_pw = verifyPw(username, password)
 
         if not correct_pw:
             retJson = {
-                "status":302,
+                "status": 302,
                 "msg": "Incorrect Password"
             }
             return jsonify(retJson)
-        #Step 4 Verify user has enough tokens
+        # Step 4 Verify user has enough tokens
         num_tokens = countTokens(username)
         if int(num_tokens) <= 0:
             retJson = {
@@ -192,66 +202,67 @@ class Similarity(Resource):
             }
             return jsonify(retJson)
 
-        #Calculate edit distance between text1, text2
+        # Calculate edit distance between text1, text2
         try:
             with open(path1) as file:
-                text1=file.read()
+                text1 = file.read()
             with open(path2) as file:
-                text2=file.read()
+                text2 = file.read()
             pass
         except:
-            retJson={"status":304,
-                     "msg":"File not found"
-                     }
+            retJson = {"status": 304,
+                       "msg": "File not found"
+                       }
             return retJson
-        #Calculate edit distance between text1, text2        
+        # Calculate edit distance between text1, text2
         text1 = nlp(text1)
         text2 = nlp(text2)
 
         ratio = text1.similarity(text2)
 
         retJson = {
-            "status":200,
+            "status": 200,
             "ratio": ratio,
-            "msg":"Similarity score calculated successfully"
+            "msg": "Similarity score calculated successfully"
         }
 
-        #Take away 1 token from user
+        # Take away 1 token from user
         current_tokens = countTokens(username)
         users.update_one({
-            "Username":username
+            "Username": username
         }, {
-            "$set":{
-                "Tokens":int(current_tokens)-1
-                }
+            "$set": {
+                "Tokens": int(current_tokens)-1
+            }
         })
 
         return jsonify(retJson)
 
+
 class Grammer_Check(Resource):
     def post(self):
-        #Step 1 get the posted data
+        # Step 1 get the posted data
         postedData = request.get_json()
-        #Step 2 is to read the data
+        # Step 2 is to read the data
         username = postedData["username"]
         password = postedData["password"]
         text = postedData["text"]
         if not UserExist(username):
             retJson = {
-                'status':301,
+                'status': 301,
                 'msg': "Invalid Username"
             }
             return jsonify(retJson)
-        #Step 3 verify the username pw match
+        # Step 3 verify the username pw match
         correct_pw = verifyPw(username, password)
 
         if not correct_pw:
             retJson = {
-                "status":302,
+                "status": 302,
                 "msg": "Incorrect Password"
             }
             return jsonify(retJson)
-        #Step 4 Verify user has enough tokens
+        # Step 4 Verify user has enough tokens
         num_tokens = countTokens(username)
         if int(num_tokens) <= 0:
             retJson = {
@@ -259,31 +270,29 @@ class Grammer_Check(Resource):
                 "msg": "You are out of tokens, please refill!"
             }
             return jsonify(retJson)
-        
-        # correction  
-        correct_text = my_tool.correct(text)  
+
+        # correction
+        correct_text = my_tool.correct(text)
 
         retJson = {
-            "status":200,
-            "msg":"Text Corrected Successfully !!",
+            "status": 200,
+            "msg": "Text Corrected Successfully !!",
             "Original Text": text,
             "Text after correction": correct_text
         }
 
-        #Take away 1 token from user
+        # Take away 1 token from user
         current_tokens = countTokens(username)
         users.update_one({
-            "Username":username
+            "Username": username
         }, {
-            "$set":{
-                "Tokens":int(current_tokens)-1
-                }
+            "$set": {
+                "Tokens": int(current_tokens)-1
+            }
         })
 
-        return retJson 
-        # printing matches  
-        
-
+        return retJson
+        # printing matches
 
 
 class Refill(Resource):
@@ -304,132 +313,136 @@ class Refill(Resource):
         correct_pw = "abc123"
         if not password == correct_pw:
             retJson = {
-                "status":304,
+                "status": 304,
                 "msg": "Invalid Admin Password"
             }
             return jsonify(retJson)
 
-        #MAKE THE USER PAY!
+        # MAKE THE USER PAY!
         users.update_one({
-            "Username":username
+            "Username": username
         }, {
-            "$set":{
-                "Tokens":refill_amount
-                }
+            "$set": {
+                "Tokens": refill_amount
+            }
         })
 
         retJson = {
-            "status":200,
+            "status": 200,
             "msg": "Refilled successfully"
         }
         return jsonify(retJson)
 
+
 class Summarize(Resource):
     def post(self):
-        posted_data=request.get_json()
-        username=posted_data["username"]
-        password=posted_data["password"]
-        document_path=posted_data["path"]
-        number_of_sentences=int(posted_data["sentences"])
+        posted_data = request.get_json()
+        username = posted_data["username"]
+        password = posted_data["password"]
+        document_path = posted_data["path"]
+        number_of_sentences = int(posted_data["sentences"])
         # verify username
         if not UserExist(username):
-            retJson={"status":301,
-            "msg":"Invalid Username"
-            }
+            retJson = {"status": 301,
+                       "msg": "Invalid Username"
+                       }
             return jsonify(retJson)
         # verify password
-        correct_pw=verifyPw(username,password)
-        
+        correct_pw = verifyPw(username, password)
+
         if not correct_pw:
-            retJson={"status":302,
-                     "msg":"Incorrect Password"
-                     }
+            retJson = {"status": 302,
+                       "msg": "Incorrect Password"
+                       }
             return jsonify(retJson)
         # verify user has enough tokens
-        num_tokens=countTokens(username)
-        if int(num_tokens<=0):
-            retJson={"status":303,
-                     "msg":"You are out of tokens"
-                     }
+        num_tokens = countTokens(username)
+        if int(num_tokens) <= 0:
+            retJson = {"status": 303,
+                       "msg": "You are out of tokens"
+                       }
             return jsonify(retJson)
-        
+
         # summarize the text finally
-        text,summarized_text=summarize_text(document_path,number_of_sentences)
-        if text=="" and summarized_text=="":
-            retJson={"status":304,
-                     "msg":"File not found"
-                     }
+        text, summarized_text = summarize_text(
+            document_path, number_of_sentences)
+        if text == "" and summarized_text == "":
+            retJson = {"status": 304,
+                       "msg": "File not found"
+                       }
             return jsonify(retJson)
-        elif len(summarized_text)==0 and len(text)>0:
-            retJson={"status":305,
-                     "msg":"Please provide sufficient number to summarize text"
-                     }
+        elif len(summarized_text) == 0 and len(text) > 0:
+            retJson = {"status": 305,
+                       "msg": "Please provide sufficient number to summarize text"
+                       }
             return jsonify(retJson)
-        retJson={"text":text,
-                 "summarized_text":summarized_text
-                 }
-        #Take away 1 token from user
+        retJson = {"text": text,
+                   "summarized_text": summarized_text
+                   }
+        # Take away 1 token from user
         current_tokens = countTokens(username)
         users.update_one({
-            "Username":username
+            "Username": username
         }, {
-            "$set":{
-                "Tokens":int(current_tokens)-1
-                }
+            "$set": {
+                "Tokens": int(current_tokens)-1
+            }
         })
         return jsonify(retJson)
 
+
 class ReadingTime(Resource):
     def post(self):
-        posted_data=request.get_json()
-        username=posted_data["username"]
-        password=posted_data["password"]
-        document_path=posted_data["path"]
-        document_path=base_path+"\\documents"+"\\"+document_path
+        posted_data = request.get_json()
+        username = posted_data["username"]
+        password = posted_data["password"]
+        document_path = posted_data["path"]
+        document_path = base_path+"\\documents"+"\\"+document_path
         try:
-            wpm=posted_data["wpm"]
+            wpm = posted_data["wpm"]
         except:
-            wpm=120
-        
+            wpm = 120
+
         # verify username
         if not UserExist(username):
-            retJson={"status":301,
-            "msg":"Invalid Username"
-            }
+            retJson = {"status": 301,
+                       "msg": "Invalid Username"
+                       }
             return jsonify(retJson)
         # verify password
-        correct_pw=verifyPw(username,password)
-        
+        correct_pw = verifyPw(username, password)
+
         if not correct_pw:
-            retJson={"status":302,
-                     "msg":"Incorrect Password"
-                     }
+            retJson = {"status": 302,
+                       "msg": "Incorrect Password"
+                       }
             return jsonify(retJson)
         # verify user has enough tokens
-        num_tokens=countTokens(username)
-        if int(num_tokens<=0):
-            retJson={"status":303,
-                     "msg":"You are out of tokens"
-                     }
+        num_tokens = countTokens(username)
+        if int(num_tokens <= 0):
+            retJson = {"status": 303,
+                       "msg": "You are out of tokens"
+                       }
             return jsonify(retJson)
         # calculate reading time finally
         try:
             with open(document_path) as file:
-                text=file.read()
+                text = file.read()
         except:
-            retJson={"status":304,
-                     "msg":"File not found"}
+            retJson = {"status": 304,
+                       "msg": "File not found"}
             return jsonify(retJson)
-        reading_time=calculate_reading_time(text,wpm)
-        retJson={"reading time (minutes)":reading_time}
+        reading_time = calculate_reading_time(text, wpm)
+        retJson = {"reading time (minutes)": reading_time}
         return jsonify(retJson)
-    
+
+
 api.add_resource(Register, '/register')
 api.add_resource(Similarity, '/similarity')
-api.add_resource(Grammer_Check,'/grammer_check')
+api.add_resource(Grammer_Check, '/grammer_check')
 api.add_resource(Refill, '/refill')
-api.add_resource(Summarize,'/summarize')
-api.add_resource(ReadingTime,'/readingtime')
+api.add_resource(Summarize, '/summarize')
+api.add_resource(ReadingTime, '/readingtime')
 
-if __name__=="__main__":
+if __name__ == "__main__":
     app.run(host='0.0.0.0')
